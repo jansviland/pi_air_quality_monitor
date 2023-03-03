ae391d12d6e1b21ea86277a72fc7c72e990dc393
Jan Sviland <jan.sviland@systek.no>
2023-03-03 16:01:34 +0100

    remove unused

--- README.md
+++ README.md
@@ -1,35 +1,60 @@
 # Raspberry Pi Air Quality Monitor
+
 A simple air quality monitoring service for the Raspberry Pi.
 
-## Installation
-Clone the repository and run the following:
+## Setup
+
+Set access key for Azure IoT Hub:
+
 ```bash
-make install
+export IOTHUB_DEVICE_CONNECTION_STRING='HostName=air-monitor-hub.azure-devices.net;DeviceId=measuring-device-id;SharedAccessKey=XXXXXXXXX_YOUR_ACCESS_KEY_XXXXXX
 ```
 
-## Running
-To run, use the run command:
+## Test
+
+To test the sensor, run the following command:
+
 ```bash
-make run
+python /home/pi/git/pi_air_quality_monitor/scripts/getMeasurement.py
 ```
 
-## Architecture
-This project uses python, flask, docker-compose and redis to create a simple web server to display the latest historical values from the sensor.
-
-## Example Data
-Some example data you can get from the sensor includes the following:
+This should print out something like:
 
 ```json
 {
-    "device_id": 13358,
-    "pm10": 10.8,
-    "pm2.5": 4.8,
-    "timestamp": "2021-06-16 22:12:13.887717"
+  "device_id": "your device id",
+  "pm10": 10.8,
+  "pm2.5": 4.8
 }
 ```
 
-The sensor reads two particulate matter (PM) values.
+## Run continuously in the background and send data to azure every minute
 
-PM10 is a measure of particles less than 10 micrometers, whereas PM 2.5 is a measurement of finer particles, less than 2.5 micrometers.
+To run, use the run command:
 
-Different particles are from different sources, and can be hazardous to different parts of the respiratory system.
+```bash
+nohup python -u /home/pi/git/pi_air_quality_monitor/scripts/sendTestDataToAzure.py >> azurelog.log &
+```
+
+## Stop script
+
+To kill you script, you can use ps -aux and kill commands.
+
+```bash
+ps -aux | grep python
+```
+
+This should show something like this:
+
+```
+pi       23338  0.0  2.2  93084 21384 ?        Sl   Feb10  20:14 python -u /home/pi/git/pi_air_quality_monitor/scripts/sendTestDataToAzure.py
+```
+
+Then you can kill the process like this:
+
+```bash
+kill -9 23338
+```
+
+
+
--- scripts/sendTestDataToAzure.py
+++ scripts/sendTestDataToAzure.py
@@ -1,3 +1,4 @@
+import datetime
 import os
 import asyncio
 import time
@@ -6,25 +7,17 @@ import serial
 
 ser = serial.Serial('/dev/ttyUSB0')
 
+CLIENT_ID = "raspberry-pi-jan"
 CONNECTION_STRING = os.getenv("IOTHUB_DEVICE_CONNECTION_STRING")
-PAYLOAD = '{{"pm2": {pm2}, "pm10": {pm10}}}'
+PAYLOAD = '{{"pm2": {pm2}, "pm10": {pm10}, "client_id": "{client_id}"}}'
 
 async def main():
-    # Fetch the connection string from an environment variable
-    # conn_str = os.getenv("IOTHUB_DEVICE_CONNECTION_STRING")
 
     # Create instance of the device client using the authentication provider
     device_client = IoTHubDeviceClient.create_from_connection_string(CONNECTION_STRING)
 
     # Connect the device client.
     await device_client.connect()
-
-    # data = PAYLOAD.format(pm2=5, pm10=6)
-
-    # Send a message to the IoT hub
-    # print(f"Sending message: {data}")
-    # await device_client.send_message(data)
-    # print("Message successfully sent")
 
     while True:
         data = []
@@ -34,51 +27,19 @@ async def main():
 
         pmtwofive = int.from_bytes(b''.join(data[2:4]), byteorder='little') / 10
         pmten = int.from_bytes(b''.join(data[4:6]), byteorder='little') / 10
+        currentTime = datetime.datetime.now()
 
-        print(f"Data point: pm25 = {pmtwofive}  pm10 = {pmten}")
-
-        data = PAYLOAD.format(pm2=pmtwofive, pm10=pmten)
+        print(f"Time: {currentTime}, Data point: pm25 = {pmtwofive}, pm10 = {pmten}, client_id = {CLIENT_ID}")
+        data = PAYLOAD.format(pm2=pmtwofive, pm10=pmten, client_id=CLIENT_ID)
 
         # Send a message to the IoT hub
         print(f"Sending message: {data}")
         await device_client.send_message(data)
-        print("Message successfully sent")
 
         time.sleep(10)
-
-    # Send a single message
-    # print("Sending message...")
-    # await device_client.send_message("This is a message that is being sent")
 
     # finally, shut down the client
     await device_client.shutdown()
 
-
 if __name__ == "__main__":
     asyncio.run(main())
-
-# import json
-# import os
-# import time
-# import datetime
-# import serial
-# import redis
-# # import aqi
-# from azure.iot.device import Message
-# from azure.iot.device.aio import IoTHubDeviceClient
-
-# CONNECTION_STRING = os.environ.get('IOTHUB_DEVICE_CONNECTION_STRING')
-# PAYLOAD = '{{"pm2": {pm2}, "pm10": {pm10}}}'
-
-# print("Connecting to Azure IoT Hub...")
-# print(CONNECTION_STRING)
-# azureIoTClient = IoTHubDeviceClient.create_from_connection_string(CONNECTION_STRING)
-
-# data = PAYLOAD.format(pm2=5, pm10=6)
-# message = Message(data)
-
-# # Send a message to the IoT hub
-# print(f"Sending message: {message}")
-# azureIoTClient.send_message(message)
-
-# print("Message successfully sent")
