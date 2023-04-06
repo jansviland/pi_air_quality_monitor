88904c2de78ba51dd629ee8e7189e4cc91daed2c
Jan Sviland <jan.sviland@systek.no>
2023-04-06 07:32:46 +0200

    added logging, dependency injection and appsettings

--- src/AirQuality/App.axaml.cs
+++ src/AirQuality/App.axaml.cs
@@ -1,15 +1,43 @@
+using System;
+using System.IO;
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
 using AirQuality.ViewModels;
 using AirQuality.Views;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
 
 namespace AirQuality;
 
 public partial class App : Application
 {
+    private static IHost Host { get; set; }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        // services.AddTransient<ISolutionService, SolutionService>();
+        services.AddSingleton<MainWindow>();
+    }
+
     public override void Initialize()
     {
+        var builder = new ConfigurationBuilder();
+
+        Log.Logger = new LoggerConfiguration()
+            .ReadFrom.Configuration(BuildConfiguration(builder))
+            .Enrich.FromLogContext()
+            .CreateLogger();
+
+        Host = Microsoft.Extensions.Hosting.Host.CreateDefaultBuilder()
+            .ConfigureServices((_, services) => { ConfigureServices(services); })
+            .UseSerilog()
+            .Build();
+
+        Log.Logger.Information("Starting application...");
+
         AvaloniaXamlLoader.Load(this);
     }
 
@@ -17,12 +45,28 @@ public partial class App : Application
     {
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
         {
-            desktop.MainWindow = new MainWindow
-            {
-                DataContext = new MainWindowViewModel(),
-            };
+            desktop.MainWindow = Host.Services.GetRequiredService<MainWindow>();
+            desktop.MainWindow.DataContext = new MainWindowViewModel();
+
+            // desktop.MainWindow = new MainWindow
+            // {
+            //     DataContext = new MainWindowViewModel(),
+            // };
         }
 
         base.OnFrameworkInitializationCompleted();
+    }
+
+    private static IConfiguration BuildConfiguration(IConfigurationBuilder builder)
+    {
+        builder
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json", optional: true)
+            .AddEnvironmentVariables();
+
+        var configuration = builder.Build();
+
+        return configuration;
     }
 }
