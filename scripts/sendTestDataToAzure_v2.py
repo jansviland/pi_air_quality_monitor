85fe7203a279d5bff8908ab23c893b16e9377e1e
Jan Sviland <jan.sviland@systek.no>
2023-04-09 16:08:00 +0200

    write csv file header

--- scripts/sendTestDataToAzure_v2.py
+++ scripts/sendTestDataToAzure_v2.py
@@ -11,6 +11,7 @@ CLIENT_ID = "raspberry-pi-jan"
 CONNECTION_STRING = os.getenv("IOTHUB_DEVICE_CONNECTION_STRING")
 JSON_PAYLOAD = '{{"pm2": {pm2}, "pm10": {pm10}, "client_id": "{client_id}"}}'
 CSV_PAYLOAD = '{pm2},{pm10},{client_id},{time}'
+CSV_HEADER = 'pm2,pm10,client_id,time'
 
 def save_data_to_file(currentTime, data):
 
@@ -20,11 +21,17 @@ def save_data_to_file(currentTime, data):
     os.makedirs(base_path, exist_ok=True)
     file_path = os.path.join(base_path, "measurements.csv")
 
+    # Check if the file exists, and write the header if it doesn't
+    if not os.path.exists(file_path):
+        with open(file_path, "w") as f:
+            f.write(CSV_HEADER)
+            f.write("\n")
+
     with open(file_path, "a") as f:
         f.write(data)
         f.write("\n")
 
-    print(f"Saved data to file: {file_path}")
+    print(f"Saved data: \"{data}\" to file: \"{file_path}\"")
 
 async def main():
 
@@ -40,11 +47,14 @@ async def main():
             datum = ser.read()
             json.append(datum)
 
+        # get the data from the sensor
         pmtwofive = int.from_bytes(b''.join(json[2:4]), byteorder='little') / 10
         pmten = int.from_bytes(b''.join(json[4:6]), byteorder='little') / 10
         currentTime = datetime.datetime.now()
 
         print(f"Time: {currentTime}, Data point: pm25 = {pmtwofive}, pm10 = {pmten}, client_id = {CLIENT_ID}")
+
+        # Create the JSON and CSV payloads
         json = JSON_PAYLOAD.format(pm2=pmtwofive, pm10=pmten, client_id=CLIENT_ID)
         cvs = CSV_PAYLOAD.format(pm2=pmtwofive, pm10=pmten, client_id=CLIENT_ID, time=currentTime)
 
