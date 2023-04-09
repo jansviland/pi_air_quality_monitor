abc8c005cac1b268e544dedb7e156bcdd09217da
Jan Sviland <jan.sviland@systek.no>
2023-04-09 15:44:44 +0200

    python save everything locally

---
+++ scripts/sendTestDataToAzure_v2.py
@@ -0,0 +1,61 @@
+import datetime
+import os
+import asyncio
+import time
+from azure.iot.device.aio import IoTHubDeviceClient
+import serial
+
+ser = serial.Serial('/dev/ttyUSB0')
+
+CLIENT_ID = "raspberry-pi-jan"
+CONNECTION_STRING = os.getenv("IOTHUB_DEVICE_CONNECTION_STRING")
+JSON_PAYLOAD = '{{"pm2": {pm2}, "pm10": {pm10}, "client_id": "{client_id}"}}'
+CSV_PAYLOAD = '{pm2},{pm10},{client_id},{time}'
+
+async def main():
+
+    # Create instance of the device client using the authentication provider
+    device_client = IoTHubDeviceClient.create_from_connection_string(CONNECTION_STRING)
+
+    # Connect the device client.
+    await device_client.connect()
+
+    while True:
+        json = []
+        for index in range(0,10):
+            datum = ser.read()
+            json.append(datum)
+
+        pmtwofive = int.from_bytes(b''.join(json[2:4]), byteorder='little') / 10
+        pmten = int.from_bytes(b''.join(json[4:6]), byteorder='little') / 10
+        currentTime = datetime.datetime.now()
+
+        print(f"Time: {currentTime}, Data point: pm25 = {pmtwofive}, pm10 = {pmten}, client_id = {CLIENT_ID}")
+        json = JSON_PAYLOAD.format(pm2=pmtwofive, pm10=pmten, client_id=CLIENT_ID)
+        cvs = CSV_PAYLOAD.format(pm2=pmtwofive, pm10=pmten, client_id=CLIENT_ID)
+
+        # Save data to file
+        save_data_to_file(currentTime.year, currentTime.month, currentTime.day, currentTime, cvs)
+
+        # Send a message to the IoT hub
+        print(f"Sending message: {json}")
+        await device_client.send_message(json)
+
+        # time.sleep(10)
+
+    # finally, shut down the client
+    await device_client.shutdown()
+
+if __name__ == "__main__":
+    asyncio.run(main())
+
+def save_data_to_file(year, month, day, data):
+    base_path = f"{year}/{month}/{day}"
+    os.makedirs(base_path, exist_ok=True)
+    file_path = os.path.join(base_path, "measurements.csv")
+
+    with open(file_path, "a") as f:
+        f.write(data)
+        f.write("\n")
+
+    print(f"Saved data to file: {file_path}")
