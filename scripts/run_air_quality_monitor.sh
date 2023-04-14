0531eb9bb6f86f149367b4608169458cc35f0bc8
Jan Sviland <jan.sviland@systek.no>
2023-04-14 12:36:50 +0200

    update to shell script

--- scripts/run_air_quality_monitor.sh
+++ scripts/run_air_quality_monitor.sh
@@ -13,4 +13,4 @@ yesterday=$(date -d yesterday "+%Y/%m/%d")
 cd ~/git/pi_air_quality_monitor/src/AirQuality.Console/bin/Debug/net6.0
 
 # Run the .NET application with the yesterday's date
-dotnet AirQuality.Console.dll ~/${yesterday}/measurements.csv >> ~/AirQuality.Console.log 2>&1
+/usr/local/bin/dotnet AirQuality.Console.dll ~/${yesterday}/measurements.csv >> ~/AirQuality.Console.log 2>&1
