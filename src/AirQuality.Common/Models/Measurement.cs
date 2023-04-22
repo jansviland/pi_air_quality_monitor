7f7f71e03c7833f6908df4b34293230d508def44
Jan Sviland <jan.sviland@systek.no>
2023-04-23 00:51:57 +0200

    rename

--- src/AirQuality.Common/Models/Measurement.cs
+++ src/AirQuality.Common/Models/Measurement.cs
@@ -1,16 +1,10 @@
-﻿using System.Text.Json.Serialization;
-
-namespace AirQuality.Common.Models;
+﻿namespace AirQuality.Common.Models;
 
 public class Measurement
 {
     public double Pm2 { get; set; }
     public double Pm10 { get; set; }
-
-    [JsonPropertyName("UtcTime")]
     public DateTime UtcTime { get; set; }
     public long? UnixTime { get; set; }
-
-    [JsonPropertyName("client_id")]
-    public string? ClientId { get; set; }
+    public string ClientId { get; set; } = null!;
 }
