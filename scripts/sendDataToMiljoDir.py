945a3954c144982a8ee40b08f199069359e0379d
Jan Sviland <jan.sviland@systek.no>
2024-02-23 18:34:16 +0100

    test

--- scripts/sendDataToMiljoDir.py
+++ scripts/sendDataToMiljoDir.py
@@ -335,15 +335,16 @@ async def main():
         # when the lists contains x items, send the data to the API
         if pm10_time_values.__len__() >= 5:
             # only send between 08:00 - 16:00 monday - friday
-            if 7 <= from_time.hour <= 15 and from_time.weekday() < 5:
-                # if fromTime.hour >= 8 and fromTime.hour <= 20:
-                # Send data to API
-                # TODO: to this as a background task, so we can continue to measure while sending data
-                send_data_to_api()
-
-            else:
-                # Handle gap in data when outside of working hours
-                print("Not sending data to API, outside of working hours")
+            # if 7 <= from_time.hour <= 15 and from_time.weekday() < 5:
+            #     # if fromTime.hour >= 8 and fromTime.hour <= 20:
+            #     # Send data to API
+            #     # TODO: to this as a background task, so we can continue to measure while sending data
+            #     send_data_to_api()
+            # 
+            # else:
+            #     # Handle gap in data when outside of working hours
+            #     print("Not sending data to API, outside of working hours")
+            send_data_to_api()
 
             # Clear the lists
             pm10_time_values.clear()
